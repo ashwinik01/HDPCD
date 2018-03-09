e89c5384f91fb1a23ae7f144298f8be187737525
Andres Urrego <ingenieroandresangel@gmail.com>
2018-03-08 22:01:50 -0500

    Create basic_operations.py

---
+++ Pyspark/basic_operations.py
@@ -0,0 +1,29 @@
+training foreach
+
+## load a file in a RDD from the file system
+fileRDD = sc.textFile(file:///home/maria_dev/files/titanic.csv)
+
+## Print method 1
+for i in fileRDD.collect():
+  print(i)
+
+## Pront method 2
+def printer(x):
+  print(x)
+  
+ fileRDD.foreach(printer)
+ 
+ #Operations map, foreach
+ def increasing(x): \
+    z = x + 2 
+
+    
+numberslist = sc.parallelize(range(10,25))
+newlist = numberslist.foreach(increasing)
+newRDD = sc.parallelize(newlist)
+
+newlist = numberslist.map(lambda x: x + 2)
+newlist.foreach(printer)
+
+ 
+ 
